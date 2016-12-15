db357111d36b2449a1a7e94de1be1bf5141e4d14
钟任 <zhrren@qq.com>
2016-12-15 20:51:30 +0800

    init

---
+++ Tests/ApiResultTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+using Mark.ApiResult;
+
+namespace Tests
+{
+    public class Model
+    {
+        public string Name { get; set; }
+    }
+
+    public class ApiResultTest
+    {
+        [Fact]
+        public void ConvertTest()
+        {
+            ApiResult result = new ApiResult<Model>();
+            ApiResult<Model> modelResult = (ApiResult<Model>)result;
+        }
+    }
+}
---
+++ Tests/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Mark.ApiResult;
+
+namespace Tests
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
